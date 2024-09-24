8c9d63d8e0a278da45e955e7a667c39dbaf6c75d
hasan khaddour <hasan.b.khaddour@gmail.com>
2024-09-24 16:05:37 +0300

    Add Repository Pattern Base

---
+++ CASolution.SharedKernel/Repositories/IReadRepository.cs
@@ -0,0 +1,15 @@
+﻿using CASolution.SharedKernel.Entities;
+using CASolution.SharedKernel.Utilities;
+using System.Collections.Generic;
+
+using System.Threading.Tasks;
+
+namespace CASolution.SharedKernel.Repositories
+{
+    public interface IReadRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<List<T>> ListAsync();
+        Task<List<T>> ListAsync(ISpecification<T> spec);
+    }
+}
---
+++ CASolution.SharedKernel/Repositories/IRepository.cs
@@ -0,0 +1,57 @@
+﻿using CASolution.SharedKernel.Entities;
+using CASolution.SharedKernel.Utilities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CASolution.SharedKernel.Repositories
+{
+    /// <summary>
+    /// IRepository provides a generic interface for accessing and manipulating 
+    /// entities in the data source. It defines the standard CRUD operations and 
+    /// supports working with specifications to query data more flexibly.
+    /// </summary>
+    /// <typeparam name="T">The type of entity that the repository will manage, 
+    /// which must inherit from BaseEntity.</typeparam>
+    public interface IRepository<T> where T : BaseEntity
+    {
+        /// <summary>
+        /// Asynchronously retrieves an entity by its unique identifier. 
+        /// Optionally, a specification can be applied to modify the query behavior.
+        /// </summary>
+        /// <param name="id">The unique identifier of the entity to retrieve.</param>
+        /// <param name="specification">An optional specification to apply to the query.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the entity if found, or null otherwise.</returns>
+        Task<T> GetByIdAsync(int id, ISpecification<T> specification = null);
+
+        /// <summary>
+        /// Asynchronously retrieves a list of entities based on the provided specification.
+        /// The specification is used to apply additional query criteria.
+        /// </summary>
+        /// <param name="specification">An optional specification that defines the criteria for selecting entities.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains a collection of entities that match the criteria.</returns>
+        Task<IEnumerable<T>> ListAsync(ISpecification<T> specification=null);
+
+        /// <summary>
+        /// Asynchronously adds a new entity to the repository.
+        /// </summary>
+        /// <param name="entity">The entity to add to the repository.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the added entity.</returns>
+        Task<T> AddAsync(T entity);
+
+
+
+        /// <summary>
+        /// Asynchronously updates an existing entity in the repository.
+        /// </summary>
+        /// <param name="entity">The entity to update.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the updated entity.</returns>
+        Task<T> UpdateAsync(T entity);
+
+        // <summary>
+        /// Asynchronously deletes an entity from the repository.
+        /// </summary>
+        /// <param name="entity">The entity to delete.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        Task DeleteAsync(T entity);
+    }
+}
