867cb9308caab2d85bfa7add9a16bf93b758c2b7
hasan khaddour <hasan.b.khaddour@gmail.com>
2024-09-24 16:07:41 +0300

    Add Aggregate Root

---
+++ CASolution.SharedKernel/DomainAggregate/IAggregateRoot.cs
@@ -0,0 +1,10 @@
+﻿using CASolution.SharedKernel.Entities;
+
+namespace CASolution.SharedKernel.DomainAggregate
+{
+    // Apply this marker interface only to aggregate root entities
+    // Repositories will only work with aggregate roots, not their children
+    public class IAggregateRoot : BaseEntity
+    {
+    }
+}
