9b8e97482a7420034e5f4614eb84705adeb9b471
hasan khaddour <hasan.b.khaddour@gmail.com>
2024-09-24 15:53:12 +0300

    Add Specification and Unit of work to the utilities

---
+++ CASolution.SharedKernel/Utilities/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace CASolution.SharedKernel.Utilities
+{
+    public interface IUnitOfWork
+    {
+        Task SaveChangesAsync(CancellationToken cancellationToken = default);
+        void BeginTransaction();
+        Task Rollback();
+    }
+
+}
